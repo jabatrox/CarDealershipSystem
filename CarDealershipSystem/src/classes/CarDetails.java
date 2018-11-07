eb67b7a76fecf6997c2305c8b7431d6732fe182b
Borja Gonzalez Diaz <borjagonzalezdiaz94@gmail.com>
2018-11-07 14:22:39 -0600

    CarDetails with attributes, getters and setters

--- CarDealershipSystem/src/classes/CarDetails.java
+++ CarDealershipSystem/src/classes/CarDetails.java
@@ -1,5 +1,100 @@
 package classes;
 
 public class CarDetails {
-
+	
+	static int carId;
+	static int conId; 
+	static int factId;
+	static String carModel;
+	static String carType;
+	static String carColor;
+	static String engineType;
+	static int horsePower;
+	static double price;
+	static int kilometers;
+	static boolean sold;
+	static boolean exposed;
+	static boolean condition;
+	
+	
+	public static int getCarId() {
+		return carId;
+	}
+	public static void setCarId(int carId) {
+		CarDetails.carId = carId;
+	}
+	public static int getConId() {
+		return conId;
+	}
+	public static void setConId(int conId) {
+		CarDetails.conId = conId;
+	}
+	public static int getFactId() {
+		return factId;
+	}
+	public static void setFactId(int factId) {
+		CarDetails.factId = factId;
+	}
+	public static String getCarModel() {
+		return carModel;
+	}
+	public static void setCarModel(String carModel) {
+		CarDetails.carModel = carModel;
+	}
+	public static String getCarType() {
+		return carType;
+	}
+	public static void setCarType(String carType) {
+		CarDetails.carType = carType;
+	}
+	public static String getCarColor() {
+		return carColor;
+	}
+	public static void setCarColor(String carColor) {
+		CarDetails.carColor = carColor;
+	}
+	public static String getEngineType() {
+		return engineType;
+	}
+	public static void setEngineType(String engineType) {
+		CarDetails.engineType = engineType;
+	}
+	public static int getHorsePower() {
+		return horsePower;
+	}
+	public static void setHorsePower(int horsePower) {
+		CarDetails.horsePower = horsePower;
+	}
+	public static double getPrice() {
+		return price;
+	}
+	public static void setPrice(double price) {
+		CarDetails.price = price;
+	}
+	public static int getKilometers() {
+		return kilometers;
+	}
+	public static void setKilometers(int kilometers) {
+		CarDetails.kilometers = kilometers;
+	}
+	public static boolean isSold() {
+		return sold;
+	}
+	public static void setSold(boolean sold) {
+		CarDetails.sold = sold;
+	}
+	public static boolean isExposed() {
+		return exposed;
+	}
+	public static void setExposed(boolean exposed) {
+		CarDetails.exposed = exposed;
+	}
+	public static boolean isCondition() {
+		return condition;
+	}
+	public static void setCondition(boolean condition) {
+		CarDetails.condition = condition;
+	}
+	
+	
 }
