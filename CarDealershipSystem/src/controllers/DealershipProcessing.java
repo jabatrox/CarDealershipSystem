06746cf922b20bf9f97eab3b5c1f6e56ec216484
Xavier <Xavier@FLYING-HELL.mshome.net>
2018-11-27 22:09:49 -0600

    DealershipProcessing imports fix

--- CarDealershipSystem/src/controllers/DealershipProcessing.java
+++ CarDealershipSystem/src/controllers/DealershipProcessing.java
@@ -1,10 +1,5 @@
 package controllers;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import models.DaoModel;
 
 public class DealershipProcessing {
