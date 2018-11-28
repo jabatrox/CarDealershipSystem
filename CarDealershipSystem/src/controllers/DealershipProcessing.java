08d84bbf26d324c929da337b873812149bd605c2
Xavier <Xavier@FLYING-HELL.mshome.net>
2018-11-27 22:07:11 -0600

    DealershipProcessing fix

--- CarDealershipSystem/src/controllers/DealershipProcessing.java
+++ CarDealershipSystem/src/controllers/DealershipProcessing.java
@@ -12,33 +12,7 @@ public class DealershipProcessing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DaoModel dao = new DaoModel();
-//	    Connection c = null;
-//	    DatabaseMetaData dbm = null;
-//		try {
-//			dbm = c.getMetaData();
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	    ResultSet rs = null;
-//		try {
-//			rs = dbm.getTables(null, null, "employee", null);
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	    try {
-//			if (rs.next()) {
-//			  System.out.println("Table exists"); 
-//			} else {
-//			  System.out.println("Table does not exist"); 
-//			}
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 		dao.createTable();
-
 	}
 
 }
