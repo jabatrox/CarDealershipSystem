45a299341aaa69912dab462786f42953e966890c
Borja Gonzalez Diaz <borjagonzalezdiaz94@gmail.com>
2018-11-28 01:47:53 -0600

    more

--- CarDealershipSystem/src/application/Main.java
+++ CarDealershipSystem/src/application/Main.java
@@ -3,7 +3,9 @@ package application;
 import classes.Admin;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -12,7 +14,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.Load(getClass().getResource("/application/Login.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Login.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
